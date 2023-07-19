fe1198f9786cc75cfe476cb3927696b9b2f436c2
Bb0lt <basil.kanaan@mail.utoronto.ca>
2023-07-18 23:13:21 -0400

    base code added. includes: -frame extraction from video file at path -pretrained model

---
+++ main.py
@@ -0,0 +1,80 @@
+import cv2
+import torch
+from torchvision import models, transforms
+
+
+def extract_frames(video_path, frame_interval):
+    frames = []
+    cap = cv2.VideoCapture(video_path)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    interval_frames = int(fps * frame_interval)
+
+    for i in range(frame_count):
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        if i % interval_frames == 0:
+            frames.append(frame)
+
+    cap.release()
+    return frames
+
+
+def classify_frames(frames, model):
+    results = []
+    preprocess = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    for frame in frames:
+        input_tensor = preprocess(frame)
+        input_batch = input_tensor.unsqueeze(0).to(device)
+
+        with torch.no_grad():
+            input_batch = input_batch.to(device)
+            output = model(input_batch)
+
+        probabilities = torch.nn.functional.softmax(output[0], dim=0)
+        top_probabilities, top_indices = torch.topk(probabilities, k=5)
+
+        results.append((top_indices, top_probabilities))
+
+    return results
+
+
+def main(path):
+    # Extract frames from the video
+    frames = extract_frames(path, frame_interval=2)
+
+    # Classify the frames using the pretrained model
+    results = classify_frames(frames, model)
+
+    # Print the top predicted classes and their probabilities for each frame
+    for i, result in enumerate(results):
+        top_indices, top_probabilities = result
+        print(f"Frame {i + 1}:")
+        for j in range(len(top_indices)):
+            label_index = top_indices[j]
+            print(f"Class: {label_index}, Probability: {top_probabilities[j].item()}")
+
+
+if __name__ == '__main__':
+    # Load the pretrained model
+    model = models.resnet50(pretrained=True)
+    model.eval()  # Set the model to evaluation mode
+
+    # Move the model to the GPU if available
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+
+    while True:
+        video_path = input("Copy & Paste the path to your video: ")
+        main(video_path)
