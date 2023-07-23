9e2370bf9ebc1156cf7f061cc4efc25a4905a51a
Bb0lt <basil.kanaan@mail.utoronto.ca>
2023-07-22 22:08:09 -0400

    added identification of double, triple, quadra, and penta kills in a video

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import cv2
-import torch
-from torchvision import models, transforms
+import numpy as np
 
+VID_PATH = None
+EXTRACT_INTERVAL = 2
+MATCH_THRESH = 0.8
+SHOW_MATCH = True
 
-def format_timedelta(td):
-    """Utility function to format timedelta objects in a cool way (e.g 00:00:20.05)
-    omitting microseconds and retaining milliseconds"""
-    result = str(td)
-    try:
-        result, ms = result.split(".")
-    except ValueError:
-        return (result + ".00").replace(":", "-")
-    ms = int(ms)
-    ms = round(ms / 1e4)
-    return f"{result}.{ms:02}".replace(":", "-")
+# Template images in grayscale
+template_images = {
+    "double": cv2.imread('images of phrases\double 2.png', cv2.IMREAD_GRAYSCALE),
+    "triple": cv2.imread('images of phrases\\triple 2.png', cv2.IMREAD_GRAYSCALE),
+    "quadra": cv2.imread('images of phrases\quadra 2.png', cv2.IMREAD_GRAYSCALE),
+    "penta": cv2.imread('images of phrases\penta 2.png', cv2.IMREAD_GRAYSCALE),
+}
 
 
 def extract_frames(video_path, frame_interval):
+    """
+    Extract frames from the video at the given frame interval.
+
+    Args:
+        video_path (str): Path to the video file.
+        frame_interval (int): Interval in seconds between extracted frames.
+
+    Returns:
+        list: List of extracted frames in grayscale.
+    """
     frames = []
     cap = cv2.VideoCapture(video_path)
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -29,67 +38,70 @@ def extract_frames(video_path, frame_interval):
             break
 
         if i % interval_frames == 0:
-            print(f"\r{i}")
-            frames.append(frame)
+            # Convert the frame to grayscale
+            frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            frames.append(frame_gray)
 
     cap.release()
     return frames
 
 
-def classify_frames(frames, model):
+def show_frame_with_border(frame, key):
+    """
+    Show the frame with a border around the matched template.
+
+    Args:
+        frame (numpy.array): Grayscale image frame.
+        key (str): Key indicating the matched template.
+    """
+    result = cv2.matchTemplate(frame, template_images[key], cv2.TM_CCOEFF_NORMED)
+    loc = np.where(result >= MATCH_THRESH)
+
+    for pt in zip(*loc[::-1]):
+        bottom_right = (pt[0] + template_images[key].shape[1], pt[1] + template_images[key].shape[0])
+        cv2.rectangle(frame, pt, bottom_right, (0, 0, 255), 2)
+
+    frame = cv2.resize(frame, (frame.shape[1] // 2, frame.shape[0] // 2))
+    cv2.imshow('Frame with Border', frame)
+    cv2.waitKey(0)  # Wait until a key is pressed (0 means infinite)
+    cv2.destroyAllWindows()
+
+
+def classify_frames(frames):
+    """
+    Classify frames using template matching.
+
+    Args:
+        frames (list): List of frames in grayscale.
+
+    Returns:
+        list: List of strings indicating the matched templates and their timestamps.
+    """
     results = []
-    preprocess = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
+    for i, frame in enumerate(frames):
+        for key in template_images:
+            result = cv2.matchTemplate(frame, template_images[key], cv2.TM_CCOEFF_NORMED)
+            loc = np.where(result >= MATCH_THRESH)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(frames)
-    for frame in frames:
-        input_tensor = preprocess(frame)
-        input_batch = input_tensor.unsqueeze(0).to(device)
-
-        with torch.no_grad():
-            input_batch = input_batch.to(device)
-            output = model(input_batch)
-
-        probabilities = torch.nn.functional.softmax(output[0], dim=0)
-        top_probabilities, top_indices = torch.topk(probabilities, k=5)
-
-        results.append((top_indices, top_probabilities))
-
+            # Output True if there is a match, False otherwise
+            if len(loc[0]) > 0:
+                results.append(f"{key} found at {i * EXTRACT_INTERVAL} seconds")
+                if SHOW_MATCH:
+                    show_frame_with_border(frame.copy(), key)
+                break
     return results
 
 
-def main(path):
-    # Extract frames from the video
-    frames = extract_frames(path, frame_interval=2)
-    print(frames)
-
-    # Classify the frames using the pretrained model
-    results = classify_frames(frames, model)
-
-    # Print the top predicted classes and their probabilities for each frame
-    for i, result in enumerate(results):
-        top_indices, top_probabilities = result
-        print(f"Frame {i + 1}:")
-        for j in range(len(top_indices)):
-            label_index = top_indices[j]
-            print(f"Class: {label_index}, Probability: {top_probabilities[j].item()}")
-
-
 if __name__ == '__main__':
-    # Load the pretrained model
-    model = models.resnet50(pretrained=True)
-    model.eval()  # Set the model to evaluation mode
+    if not VID_PATH:
+        VID_PATH = input("Copy & Paste the path to your video: ")
 
-    # Move the model to the GPU if available
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = model.to(device)
+    # Extract frames from the video
+    frames = extract_frames(VID_PATH, frame_interval=EXTRACT_INTERVAL)
 
-    while True:
-        video_path = input("Copy & Paste the path to your video: ")
-        main(video_path)
+    # Classify the frames using template matching
+    results = classify_frames(frames)
+
+    # Print the matched templates and their timestamps
+    for result in results:
+        print(result)
